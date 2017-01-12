cd1703b977bb11c727b8ced2068042ed3e8bab42
CollinBoback <collinboback@Collins-MacBook-Pro.local>
2017-01-12 08:59:06 -0500

    CS111 / Fall16

---
+++ Compress.java
@@ -0,0 +1,31 @@
+public class Compress{
+
+    public static void main(String[] args) {
+
+        
+
+    }
+
+
+    public static String compress(String original) {
+        
+        StringBuilder compressed = new StringBuilder();
+        char testLetter = 0;
+        int count = 1;
+        for (int i = 0; i < original.length(); i++) {
+            if (testLetter == original.charAt(i)) {
+                count = count + 1;
+            } else {
+                compressed = count != 1 ? compressed.append(count) : compressed;
+                compressed.append(testLetter);
+                testLetter = original.charAt(i);
+                count = 1;
+            }
+        }
+        
+        compressed = count != 1 ? compressed.append(count) : compressed;
+        compressed.append(testLetter);
+        return compressed.toString().trim();
+        
+    }
+}
